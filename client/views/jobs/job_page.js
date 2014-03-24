e25155d1106fb345ec4e290197ee243186514d3b
Russell Hutson <russell@feeed.com>
2014-03-24 16:37:58 +0000

    get Update working, support 2 Markdown editors

--- client/views/jobs/job_page.js
+++ client/views/jobs/job_page.js
@@ -3,6 +3,10 @@
  */
 Template.jobPage.helpers({
   getSkills: function(input) {
+    // handle legacy Jobs
+    if (Array.isArray(input))
+      input = input[0];
+    
     if (typeof input != 'undefined') {
       var skillsArray = [];
       skillsArray = input.split(',');
@@ -13,10 +17,37 @@ Template.jobPage.helpers({
   }, // getSkills
   
   job_description_html: function() {
-    console.log(this.job_description);
-   if (typeof this.job_description !== 'undefined')
-      return marked(this.job_description);
+    var desc;
+        if (Array.isArray(this.job_description))
+          desc = this.job_description[0];
+        else 
+          desc = this.job_description;
+    
+    console.log(desc);
+   if (typeof desc !== 'undefined')
+      return marked(desc);
     else
       return  '';
   } // job_description_html
 })
+
+Template.jobPage.events({
+  'click .delete': function(e) {
+             e.preventDefault();
+             if (confirm("Delete this Job?")) {
+               var currentJobId = this._id;
+               Jobs.remove(currentJobId, function(error) {
+                if (!error) {
+                   Alerts.add('Job '+this.job_title+' / '.currentJobId+' DELETED!', 'warning', {
+                                  fadeIn: 1000, fadeOut: 1000, autoHide: 10000
+                   });
+                 } else {
+                   Alerts.add('FAILED!!: Job '+this.job_title+' / '.currentJobId+' NOT deleted!', 'danger', {
+                                  fadeIn: 1000, fadeOut: 1000, autoHide: 10000
+                   });
+                 }
+                 Router.go('jobsList');
+               }); // remove + callback
+             }
+   } // delete
+});
